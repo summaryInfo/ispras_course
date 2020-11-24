60200d8129c95ae8a4c9aca5a8e3147a0f694263
Baskov Evgeniy <j-basevgser@yandex.ru>
2020-11-24 12:33:32 +0300

    Support input file spcification

--- main.c
+++ main.c
@@ -7,22 +7,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+enum retcode {
+    ERC_WRONG_EXPR = 1,
+    ERC_NO_IN_FILE = 2,
+    ERC_NO_OUT_FILE = 3,
+    ERC_WRONG_PARAM = 4,
+};
+
 _Noreturn void usage(const char *argv0) {
     printf("Usage:\n"
-           "\t%s [-f <format>] [-o <file>] <expr>\n"
+           "\t%s [-f <format>] [-o <outfile>] <expr>\n"
+           "\t%s [-f <format>] [-o <outfile>] -i <infile>\n"
            "<format> is one of tex, string, graph\n"
-           "Default <file> is stdout\n", argv0);
-    exit(0);
+           "Default <outfile> is stdout\n", argv0, argv0);
+    exit(ERC_WRONG_PARAM);
 }
 
 int main(int argc, char **argv) {
     enum format fmt = fmt_string;
-    const char *file = NULL;
+    const char *output = NULL, *input = NULL;
 
-    for (int c; (c = getopt(argc, argv, "o:f:")) != -1;) {
-        if (c == 'f') {
-
-        } else usage(argv[0]);
+    for (int c; (c = getopt(argc, argv, "i:o:f:")) != -1;) {
         switch (c) {
         case 'f':
             if (!strcmp(optarg, "tex"))
@@ -34,24 +39,27 @@ int main(int argc, char **argv) {
             else usage(argv[0]);
             break;
         case 'o':
-            file = optarg;
+            output = optarg;
+            break;
+        case 'i':
+            input = optarg;
             break;
         default:
             usage(argv[0]);
         }
     }
 
-    if (argc <= optind || !argv[optind]) usage(argv[0]);
+    if (!input && (argc <= optind || !argv[optind])) usage(argv[0]);
 
-    char *buf = argv[optind];
-    FILE *in = fmemopen(buf, strlen(argv[optind]), "r");
-    if (!in) return 1;
+    FILE *in = input ? fopen(input, "r") :
+                       fmemopen(argv[optind], strlen(argv[optind]), "r");
+    if (!in) return ERC_NO_IN_FILE;
 
-    FILE *out = file ? fopen(file, "w") : stdout;
-    if (!out) return 2;
+    FILE *out = output ? fopen(output, "w") : stdout;
+    if (!out) return ERC_NO_OUT_FILE;
 
     struct expr *exp = parse_tree(in);
-    if (!exp) return 1;
+    if (!exp) return ERC_WRONG_EXPR;
 
     dump_tree(out, fmt, exp);
 
